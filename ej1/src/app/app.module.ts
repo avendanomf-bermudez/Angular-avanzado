5afe2665bacbc819594da919c57515519356536e
Miguel Fabian Avendano Bermudez <ECM6802I@claro.co>
2020-01-10 17:53:14 -0500

    primer commit

---
+++ ej1/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainpageComponent } from './pages/mainpage.component';
+import { AboutComponent } from './pages/about.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MainpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
